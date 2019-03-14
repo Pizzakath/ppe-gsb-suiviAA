53d7cabefee05d9bd207523522302d2e1e56e67f
Milouz2 <emilien.wacrenier@epsi.fr>
2019-03-14 16:48:40 +0100

    page accueil basique

--- app/src/main/java/com/example/emilien/suiviaa/MainActivity.java
+++ app/src/main/java/com/example/emilien/suiviaa/MainActivity.java
@@ -2,13 +2,23 @@ package com.example.emilien.suiviaa;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.Button;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+
+    EditText login;
+    EditText password;
+    Button connexion;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        int a = 0;
+
+        login = (EditText) findViewById(R.id.main_login);
+        password = (EditText) findViewById(R.id.main_password);
+        connexion = (Button) findViewById(R.id.main_connexion);
+
     }
 }
