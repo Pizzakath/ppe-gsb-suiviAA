6d7a16cbeedafb2dc34a2a561e314f0f210af8e0
Milouz2 <emilien.wacrenier@epsi.fr>
2019-03-28 15:46:14 +0100

    Premier Commit de la journee

--- app/src/main/java/com/example/emilien/suiviaa/MainActivity.java
+++ app/src/main/java/com/example/emilien/suiviaa/MainActivity.java
@@ -1,24 +1,33 @@
 package com.example.emilien.suiviaa;
 
+import android.content.Intent;
+import android.databinding.DataBindingUtil;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
+import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 
-public class MainActivity extends AppCompatActivity {
+import com.example.emilien.suiviaa.databinding.ActivityMainBinding;
 
-    EditText login;
-    EditText password;
-    Button connexion;
+public class MainActivity extends AppCompatActivity {
+    ActivityMainBinding binding;
+
+
+    public void connexion(View v){
+        Log.d("MonTest", "Fonction Connexion");
+    }
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main);
 
-        login = (EditText) findViewById(R.id.main_login);
-        password = (EditText) findViewById(R.id.main_password);
-        connexion = (Button) findViewById(R.id.main_connexion);
+
+
+
+
 
     }
 }
